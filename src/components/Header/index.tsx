81542bd2b94329068cb31b9444831b53ac4eed12
Alan Neres <alanneres10@hotmail.com>
2022-07-21 00:18:46 -0300

    feat: add eslint configuration

--- src/components/Header/index.tsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <div>
-      <h1>Menu titulo 2</h1>
-    </div>
-  );
-}
-
-export { Header };
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,13 @@
+function Main({
+  title = 'NextJS - Boilerplate',
+  description = 'NextJS, Typescript e Styled-Components'
+}) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h2>{description}</h2>
+    </div>
+  );
+}
+
+export { Main };
